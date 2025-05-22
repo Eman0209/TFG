8fec8401077c52674f01c635db470a0a75804912
Eman0209 <rosa.hortelano@estudiantat.upc.edu>
2025-05-22 22:23:34 +0200

    eliminar warnings

--- app/lib/presentation/screens/mystery/time_service.dart
+++ app/lib/presentation/screens/mystery/time_service.dart
@@ -1,4 +1,10 @@
+import 'package:app/presentation/presentation_controller.dart';
+import 'package:logging/logging.dart';
+
 class TimerService {
+
+  final Logger _logger = Logger('TimerService');
+  
   static final TimerService _instance = TimerService._internal();
 
   factory TimerService() => _instance;
@@ -23,6 +29,19 @@ class TimerService {
 
   void reset() {
     _stopwatch.reset();
+  }
+
+  Future<void> persistElapsedTime(PresentationController controller, String routeId) async {
+    try {
+      stop(); // Ensure stopwatch is stopped
+      Duration existingDuration = await controller.getStartedRouteDuration(routeId);
+      final totalDuration = (existingDuration) + _stopwatch.elapsed;
+
+      await controller.updateStartedRouteDuration(routeId, totalDuration);
+      reset();
+    } catch (e) {
+      _logger.severe('Error persisting elapsed time: $e');
+    }
   }
 
   Duration get elapsed => _stopwatch.elapsed;
