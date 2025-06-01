5208b5c7001fa472204a2a77d4849a240317fc49
Eman0209 <rosa.hortelano@estudiantat.upc.edu>
2025-06-01 19:20:47 +0200

    integration test

---
+++ app/test/integration_test/app_test.dart
@@ -0,0 +1,15 @@
+import 'package:integration_test/integration_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:app/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  /*testWidgets('', (WidgetTester tester) async {
+    app.main(); // starts your app
+    await tester.pumpAndSettle();
+
+    // Now interact with your app using tester:
+    // tap buttons, enter text, scroll, verify widgets etc.
+  });*/
+}
