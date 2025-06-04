7f298484d88616aa82d014c3eba06e5996b785eb
Eman0209 <rosa.hortelano@estudiantat.upc.edu>
2025-06-04 16:49:20 +0200

    rename

--- app/lib/presentation/presentation_controller.dart
+++ app/lib/presentation/presentation_controller.dart
@@ -1,6 +1,6 @@
 import 'package:app/presentation/screens/mystery/activities/first_activity.dart';
 import 'package:app/presentation/screens/mystery/activities/second_activity.dart';
-import 'package:app/presentation/screens/mystery/activities/third_actuvity.dart';
+import 'package:app/presentation/screens/mystery/activities/third_activity.dart';
 import 'package:app/presentation/screens/mystery/step_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/easy_localization.dart';
