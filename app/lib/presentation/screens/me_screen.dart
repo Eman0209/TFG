839d8f3395c153290d5286641030ad4348f4b4d3
Eman0209 <rosa.hortelano@estudiantat.upc.edu>
2025-06-16 19:03:23 +0200

    arreglar logout

--- app/lib/presentation/screens/me_screen.dart
+++ app/lib/presentation/screens/me_screen.dart
@@ -67,6 +67,14 @@ class _PerfilPageState extends State<PerfilPage> {
             const SizedBox(height: 10),
             _buildMenuItem(
               icon: Icons.warning_amber_outlined,
+              text: 'logout'.tr(),
+               onTap: () {
+                _presentationController.logout(context);
+              }
+            ),
+            const SizedBox(height: 10),
+            _buildMenuItem(
+              icon: Icons.warning_amber_outlined,
               text: 'howToPlay'.tr(),
                onTap: () {
                 _presentationController.howToPlayScreen(context);
